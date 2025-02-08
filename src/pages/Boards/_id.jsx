c86741ede9462b43acd9ebc55608ab04eef6faab
QUANG221222 <nguyennhatquang.2509@gmail.com>
2025-02-08 09:35:13 +0700

    Axios Call API

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,15 +1,26 @@
+import { useState, useEffect } from 'react'
 import { Container } from '@mui/material'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoxContent from './BoardContent/BoardContent'
-import { mockData } from '~/apis/mock-data'
+// import { mockData } from '~/apis/mock-data'
+import { fetchBoardDetailsAPI } from '~/apis'
 
 function Board() {
+  const [board, setBoard] = useState(null)
+  useEffect(() => {
+    // Tạm thời fix cứng boardId, flow chuẩn chỉnh về sau khi học nâng cao trực tiếp, chúng ta sẽ sử dụng react-router-dom để lấy boardId từ URL
+    const boardId = '67a5c88842b496c3b20e0567'
+    //Call API
+    fetchBoardDetailsAPI(boardId).then((board) => {
+      setBoard(board)
+    })
+  })
   return (
     <Container disableGutters maxWidth="false" sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={mockData?.board} />
-      <BoxContent board={mockData?.board} />
+      <BoardBar board={board} />
+      <BoxContent board={board} />
     </Container>
   )
 }
